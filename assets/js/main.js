77d922751b3d0d15713db40e4690cc9fb0985b4e
nish <nish159@outlook.com>
2023-09-02 16:57:41 -0400

    complete project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,59 +1,58 @@
-/*=================== SHOW MENU ===================*/
+/*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
 
-/*=================== MENU SHOW ===================*/
-/* VALIDATE IF CONSTANT EXISTS */
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
 if(navToggle){
     navToggle.addEventListener('click', () =>{
         navMenu.classList.add('show-menu')
     })
 }
 
-/*=================== MENU HIDDEN ===================*/
-/* VALIDATE IF CONTANT EXISTS */
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
 if(navClose){
     navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
     })
 }
 
-/*=================== REMOVE MENU MOBILE ===================*/
+/*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
-    // WHEN WE CLICK ON EACH NAV__LINK, WE REMOVE THE SHOW-MENU CLASS
+    // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-
-/*=================== CHANGE BACKGROUND HEADER ===================*/
+/*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader(){
     const header = document.getElementById('header')
-    // WHEN THE SCROLL IS GREATER THAN 50 VIEWPORT HEIGHT, ADD THE SCROLL-HEADER CLASS TO THE HEADER TAG
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
     if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
-/*=================== SHOW SCROLL UP ===================*/
+/*=============== SHOW SCROLL UP ===============*/ 
 function scrollUp(){
     const scrollUp = document.getElementById('scroll-up');
-    // WHEN THE SCROLL IS HIGHER THAN 200 VIEWPORT HEIGHT, ADD THE SHOW-SCROLL CLASS TO THE A TAG WITH THE SCROLL-TOP CLASS
+    // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
     if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
 
-/*=================== SCROLL SECTIONS ACTIVE LINK ===================*/
-const sections = document.querySelectorAll('section[id')
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
 
 function scrollActive(){
     const scrollY = window.pageYOffset
 
     sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight;
+        const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute('id')
 
@@ -66,5 +65,20 @@ function scrollActive(){
 }
 window.addEventListener('scroll', scrollActive)
 
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
+})
 
-/*=================== SCROLL REVEAL ANIMATION ===================*/
+sr.reveal(`.home__header, .section__title`,{delay: 600})
+sr.reveal(`.home__footer`,{delay: 700})
+sr.reveal(`.home__img`,{delay: 900, origin: 'top'})
+
+sr.reveal(`.sponsor__img, .products__card, .footer__logo, .footer__content, .footer__copy`,{origin: 'top', interval: 100})
+sr.reveal(`.specs__data, .discount__animate`,{origin: 'left', interval: 100})
+sr.reveal(`.specs__img, .discount__img`,{origin: 'right'})
+sr.reveal(`.case__img`,{origin: 'top'})
+sr.reveal(`.case__data`)
